b17d79825f21c84a8997512149d86ac2123d4c65
Matt Santucci <info@skypoint.media>
2025-09-10 16:23:47 -0400

    Added Entra Auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-
+import LogoutButton from "@/components/ui/buttons/LogoutButton";
 export default function Dashboard() {
   const session = useSession();
   return (
     <div>
       <p>dashboard page </p>
       <p>User email: {session.data?.user?.email}</p>
+        <LogoutButton />
     </div>
   );
 }
